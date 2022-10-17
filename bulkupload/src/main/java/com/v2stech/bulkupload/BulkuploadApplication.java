bbe60f71bf5380266844af3f6cb724828cdc2a11
Ankit_v2s <ankit.v2stech@gmail.com>
2022-10-17 14:52:21 +0530

    Initial Structure commit

---
+++ bulkupload/src/main/java/com/v2stech/bulkupload/BulkuploadApplication.java
@@ -0,0 +1,13 @@
+package com.v2stech.bulkupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkuploadApplication.class, args);
+	}
+
+}
---
+++ bulkupload/src/test/java/com/v2stech/bulkupload/BulkuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v2stech.bulkupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
