974b8f3630f99462ab6eb2595f58575cf11eb2db
sachidanand yadav <sachidanand.v2stech@gmail.com>
2022-10-17 17:15:32 +0530

    UI

--- bulkupload/src/main/java/com/v2stech/bulkupload/controller/UploadController.java
+++ bulkupload/src/main/java/com/v2stech/bulkupload/controller/UploadController.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.v2stech.bulkupload.service.UploadService;
 
@@ -13,7 +15,13 @@ public class UploadController {
 	
 	@Autowired
 	UploadService uploadService;
-
+	
+	
+	@RequestMapping("/")
+	public ModelAndView getWelcomePage() {
+		return new ModelAndView("index");
+	}
+	
 	@PostMapping("/read")
 	public String uploadFile(@RequestParam(value = "file", required = true) MultipartFile file) throws IOException {
 		return uploadService.uploadFile(file.getOriginalFilename()).toString();
